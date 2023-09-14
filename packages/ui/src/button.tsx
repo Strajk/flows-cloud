946ae4f64cffb098d6e529008173c497549fde0d
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-14 07:30:29 +0200

    Add storybook

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -35,7 +35,7 @@ export interface ButtonProps
   asChild?: boolean;
 }
 
-export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
@@ -45,4 +45,4 @@ export const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
 );
 Button.displayName = "Button";
 
-// export { Button, buttonVariants };
+export { Button, buttonVariants };
