92af7398452fdaedb9cc9cddb61e08652f7724dc
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-17 10:27:59 +0200

    feat: add icons

---
+++ packages/icons/src/close16.tsx
@@ -0,0 +1,12 @@
+export function Close16(props: React.SVGProps<SVGSVGElement>): JSX.Element {
+  return (
+    <svg height={16} viewBox="0 0 16 16" width={16} xmlns="http://www.w3.org/2000/svg" {...props}>
+      <path
+        clipRule="evenodd"
+        d="M13.749 3.463a.857.857 0 10-1.212-1.212L8 6.788 3.463 2.25a.857.857 0 00-1.212 1.212L6.788 8 2.25 12.537a.857.857 0 101.212 1.212L8 9.212l4.537 4.537a.857.857 0 101.212-1.212L9.212 8l4.537-4.537z"
+        fill="currentColor"
+        fillRule="evenodd"
+      />
+    </svg>
+  );
+}
