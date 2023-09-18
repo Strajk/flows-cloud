54ea64e5193897035bbdf5ba5871b62f20f19016
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-18 09:15:11 +0200

    feat(web): add metadata

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,4 +1,26 @@
+import type { Metadata } from "next";
 import "./globals.css";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://flows.sh"),
+  title: "Flows",
+  description: "A better way to onboard users and drive product adoption.",
+  openGraph: {
+    type: "website",
+    title: "Flows",
+    description: "A better way to onboard users and drive product adoption.",
+    images: "/og.png",
+    url: "/",
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Flows",
+    description: "A better way to onboard users and drive product adoption.",
+    images: "/og.png",
+  },
+  keywords: ["flows", "onboarding", "product adoption", "user onboarding", "user adoption"],
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,4 +1,9 @@
+import type { Metadata } from "next";
 import { Button } from "ui";
+
+export const metadata: Metadata = {
+  alternates: { canonical: "https://flows.sh" },
+};
 
 export default function Page(): JSX.Element {
   return <Button>Click me</Button>;
