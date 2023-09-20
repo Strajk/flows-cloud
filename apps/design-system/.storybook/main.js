85784356c38e3b70025e6b2af0cba8952d7f12a2
Vojtěch Vidra <vojtechvidra@gmail.com>
2023-09-20 07:19:36 +0200

    feat: move stories from design-system to ui

--- apps/design-system/.storybook/main.js
+++ apps/design-system/.storybook/main.js
@@ -10,7 +10,7 @@ function getAbsolutePath(value) {
 
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../stories/*.stories.tsx", "../stories/**/*.stories.tsx"],
+  stories: ["../../../packages/ui/src/**/*.stories.tsx"],
   addons: [
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
